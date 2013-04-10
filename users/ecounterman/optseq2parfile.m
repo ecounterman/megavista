0bc0035a721b4556308fe532752c16bcf73619ec
Elizabeth Counterman <e.counterman@gmail.com>
2013-04-10 15:56:07 -0700

    Adding new script optseq2par

---
+++ users/ecounterman/optseq2parfile.m
@@ -0,0 +1,33 @@
+function optseq2parfile(subjectID,scanDate,run)
+
+optseqFile = uigetfile('*.*');
+fid = fopen(['Stimuli/optseq/' optseqFile]);
+stimOrder = textscan(fid, '%f%f%f%f%s');
+fclose(fid);
+% subjectID = input('Subject ID: ','s');
+% scanDate = input('Scan date: ', 's');
+% run = input('Run number: ');
+nEvents = length(stimOrder{1});
+trialNum = 1;
+
+% write text file
+fileName = sprintf('Stimuli/parfiles/%s_%s_singletrial_run%02d.par', subjectID, scanDate, run);
+fid = fopen(fileName,'w');
+for iEvent = 1:nEvents
+    if strcmp(stimOrder{5}(iEvent),'NULL')
+    else
+        fprintf(fid, '%3.2f\t%d\t%s', stimOrder{1}(iEvent), trialNum, char(stimOrder{5}(iEvent)));
+        fprintf(fid, '\n');
+        trialNum = trialNum + 1;
+    end
+end
+status = fclose(fid);
+
+% report
+if status==0
+    fprintf('Wrote par file %s.\n', fileName)
+else
+    fprintf('Check par file %s.\n', fileName)
+end
+
+end
